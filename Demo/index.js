9b4ca26b857575711b8cf091b63a33d6b5736613
vaishnavikancharlapalli <vaishukancharlapalli2000@gmail.com>
2021-09-04 15:06:52 +0530

    final commit

--- Demo/index.js
+++ Demo/index.js
@@ -7,6 +7,7 @@ app.get('/getuser',(req,res) =>
     res.send("Wlecome user")
     console.log("Hello I am sending response to the user Thank you (:")
     console.log("I am doing changes on the code for reflecting the tasks")
+    console.log("I am checking that its working or not after creation of docker image")
 })
 
 
